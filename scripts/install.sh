c6e96b80ae52ee19a4754821f7ed935da6d56a59
Matt Fletcher <fletcherm@gmail.com>
2025-08-16 13:38:54 -0400

    robustify and dedupe install script a bit.

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,9 +2,19 @@
 
 set -x
 
+REPO_SCRIPTS_DIR=~/git/pbc-scripts/scripts
+SCRIPTS="backup-to-pbs.sh list-pbs-snapshots.sh pbs-environment.sh restore-pbs-backup.sh"
+
+for script in $SCRIPTS; do
+  if [ ! -f "$REPO_SCRIPTS_DIR/$script" ]; then
+    echo "Error: $REPO_SCRIPTS_DIR/$script does not exist. Aborting installation."
+    exit 1
+  fi
+done
+
 mkdir -p ~/bin
 cd ~/bin || exit
 
-for script in backup-to-pbs.sh list-pbs-snapshots.sh pbs-environment.sh restore-pbs-backup.sh; do
-  ln -s ~/git/pbc-scripts/scripts/"$script" "$script"
+for script in $SCRIPTS; do
+  ln -s "$SCRIPTS_DIR/$script" "$script"
 done
