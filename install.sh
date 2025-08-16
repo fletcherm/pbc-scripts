b7667f32cf3eb6b19873aa5288901ec3601a39d2
Matt Fletcher <fletcherm@gmail.com>
2025-08-16 13:25:14 -0400

    shellcheck

--- install.sh
+++ install.sh
@@ -1,11 +1,10 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 set -x
 
-mkdir ~/bin
-cd ~/bin
+mkdir -p ~/bin
+cd ~/bin || exit
 
-for script in backup-to-pbs.sh list-pbs-snapshots.sh pbs-environment.sh restore-pbs-backup.sh
-do
-  ln -s ~/git/pbc-scripts/$script
+for script in backup-to-pbs.sh list-pbs-snapshots.sh pbs-environment.sh restore-pbs-backup.sh; do
+  ln -s ~/git/pbc-scripts/"$script" "$script"
 done
