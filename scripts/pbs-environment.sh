85e087d021e2660c143d350eb65f3903c7c0fd83
Matt Fletcher <fletcherm@gmail.com>
2025-08-18 12:36:14 -0400

    Add PBS_PASSWORD check to the environment checks.

--- scripts/pbs-environment.sh
+++ scripts/pbs-environment.sh
@@ -14,6 +14,7 @@ fi
 : "${PBS_BACKUP_DIR:?Variable PBS_BACKUP_DIR not set or empty}"
 : "${PBS_BACKUP_NAME:?Variable PBS_BACKUP_NAME not set or empty}"
 : "${PBS_NAMESPACE:?Variable PBS_NAMESPACE not set or empty}"
+: "${PBS_PASSWORD:?Variable PBS_PASSWORD not set or empty}"
 : "${PBS_REPOSITORY:?Variable PBS_REPOSITORY not set or empty}"
 
 "$PBC" login
